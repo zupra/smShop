8eab620d2286d22a1ca14ceee13626b61db68f37
Dima <dima5447298@gmail.com>
2019-06-14 19:16:56 +0300

    Start conf

---
+++ client/pages/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+
+.wrap
+  h1 HELLO
+  pre {{$data}}
+
+</template>
+
+<script>
+const apiUrl = process.env.API_URL || 'http://localhost:1337'
+export default {
+  // components: {}
+  data() {
+    return {}
+  },
+  async asyncData({ app }) {
+    const { data } = await app.$axios.$get(`${apiUrl}/restaurants`)
+    return {
+      data
+    }
+  }
+}
+</script>
+
+<style></style>
